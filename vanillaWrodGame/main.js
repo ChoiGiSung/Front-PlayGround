5db50238a20caef677abdd6841c46fb68008fa2a
gisungchoi <chlrltjd159@naver.com>
2022-08-27 18:05:22 +0900

    js

---
+++ vanillaWrodGame/main.js
@@ -0,0 +1,58 @@
+// 단어 변경
+//1. 단어 입력 시 이벤트로 결과 변경
+
+const $ = document.querySelector.bind(document)
+
+const defualtTime = 5
+const word = $(".word")
+const input = $(".input")
+const socoreView = $(".score")
+const timeView = $(".time")
+
+let time = defualtTime
+let timeInterval
+let isPlaying = true
+let words = []
+let score = 0
+
+init()
+
+function init(){
+    initWords()
+    word.innerText = words[0]
+    input.addEventListener("input",inputHandler.bind())
+    timeInterval = setInterval(countDown,1000)
+}
+
+function initWords(){
+    words = ["a","b","c"]
+}
+
+function inputHandler(){
+    let result = wordMatch(word.innerText, input.value)
+    if(result){
+        score += 1
+        socoreView.innerText = score
+        input.innerText = ""
+        time = defualtTime
+        changeWord()
+    }
+}
+
+function wordMatch(text, target){
+   return text === target
+}
+
+function countDown(){
+    time > 0 ? time-- : isPlaying =false
+    if(!isPlaying){
+        clearInterval(timeInterval)
+        alert("game over")
+    }
+    timeView.innerText = time
+ }
+
+function changeWord(){
+    const random = Math.floor(Math.random() * words.length)
+    word.innerText = words[random]
+}

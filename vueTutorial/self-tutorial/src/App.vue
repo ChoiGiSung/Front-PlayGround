f6df9a9a63f0bc7fe22327e7e6e75cb9e2298b9b
DESKTOP-FL6VBV6\chlrl <chlrltjd159@naver.com>
2022-08-29 19:29:46 +0900

    연습

---
+++ vueTutorial/self-tutorial/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+      <Header></Header>
+      <div>
+        <router-view></router-view>
+      </div>
+  </div>
+</template>
+
+<script>
+import Header from "./components/Header"
+
+export default {
+  name: 'App',
+  components : {
+    Header,
+  }
+}
+</script>
+
+
---
+++ vueTutorial/self-tutorial/src/components/Home.vue
@@ -0,0 +1,58 @@
+<template>
+    <div>
+        <h1>홈 {{title1}}!</h1>
+        <input type="text" v-model="input1">
+        <button type="button" @click="getData">Get</button>
+        <button type="button" @click="setData">Set</button>
+
+        <select class="form-control" v-model="region" @change="changeRegion" v-show="selectShow">
+            <option :key="i" :value="d.name" v-for="(d,i) in options">
+                {{d.title}}
+            </option>
+        </select>
+
+        <table v-if="tableShow">
+            <tr :key="i" v-for="(d,i) in options">
+                <td>{{d.name}}</td>
+                <td>{{d.title}}</td>
+            </tr>
+        </table>
+    </div>
+</template>
+<script>
+
+export default {
+  name: "Home",
+  data(){
+    return{
+        title1 : "타이틀1",
+        title2 : "타이틀2",
+        input1 : "인풋1",
+        options: [
+            {name : "S" ,title : "서울"},
+            {name : "J" ,title : "제주"},
+            {name : "B" ,title : "부산"},
+        ],
+        region : "B",
+        tableShow: false,
+        selectShow: true
+    }
+  },
+  methods:{
+    getData(){
+        alert(this.input1)
+    },
+    setData(){
+        this.input1 = "12345"
+    },
+    changeRegion(){
+        alert(this.region)
+    }
+  },
+  watch:{
+    input1(){
+        alert("데이터 바뀜")
+    }
+  }
+}
+</script>

4b88b0ebf98e2cc2f116aff248d6bcba4632bc81
DESKTOP-FL6VBV6\chlrl <chlrltjd159@naver.com>
2022-09-05 23:03:33 +0900

    add to localStorage

--- vueTutorial/todo-list/src/components/TodoList.vue
+++ vueTutorial/todo-list/src/components/TodoList.vue
@@ -1,10 +1,30 @@
 <template>
-  <div></div>
+  <div>
+    <ul>
+      <li v-for="item in todoList" :key="item">{{item}}</li>
+    </ul>
+  </div>
 </template>
 
 <script>
 export default {
-    name: "TodoList"
+    name: "TodoList",
+    data: function(){
+      return {
+        todoList:[]
+      }
+    },
+    created: function(){
+      for(let i = 0; i < localStorage.length; i++){
+         let item = localStorage.key(i)
+         this.todoList.push(item)
+      }
+    }
+    ,
+    methods : {
+
+    }
+
 }
 </script>
 

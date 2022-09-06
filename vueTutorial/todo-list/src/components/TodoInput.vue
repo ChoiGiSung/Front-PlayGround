50d6f486aef4afa1836b2b2625f21faa4300cad3
DESKTOP-FL6VBV6\chlrl <chlrltjd159@naver.com>
2022-09-06 23:33:36 +0900

    add modal

--- vueTutorial/todo-list/src/components/TodoInput.vue
+++ vueTutorial/todo-list/src/components/TodoInput.vue
@@ -1,22 +1,42 @@
 <template>
   <div>
     <input type="text" v-model="input" @keyup.enter="saveInput">
+    <ModalView v-if="modalIsShow" @close="modalIsShow = false">
+      <template v-slot:header>
+        경고
+        <button @click="modalIsShow = !modalIsShow">닫기</button>
+      </template>
+      <template v-slot:footer>
+        슬롯이 변경
+      </template>
+    </ModalView>
     <button @click="saveInput">추가</button>
   </div>
 </template>
 
 <script>
+import ModalView from './common/ModalView.vue'
+
 export default {
     name: "TodoInput",
     data: function(){
       return{
-        input:""
+        input:"",
+        modalIsShow: false
       }
+    },
+    components:{
+      ModalView
     },
     methods:{
       saveInput(){
-        this.$emit('saveInput',this.input)
-        this.initInput()
+        if(this.input !== ''){
+          this.$emit('saveInput',this.input)
+          this.initInput()
+        }else{
+          this.modalIsShow = !this.modalIsShow
+        }
+
       },
       initInput(){
         this.input = ""

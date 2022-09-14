66f47f9381bdd06ea0ea890924f6228a0baa4ef8
DESKTOP-FL6VBV6\chlrl <chlrltjd159@naver.com>
2022-09-14 22:21:33 +0900

    init app default component

--- vueTutorial/self-todo-list/src/App.vue
+++ vueTutorial/self-todo-list/src/App.vue
@@ -1,14 +1,26 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
+    <AppHeader></AppHeader>
+    <AppInput></AppInput>
+    <AppList></AppList>
+    <AppFooter></AppFooter>
   </div>
 </template>
 
 <script>
+import AppHeader from './components/AppHeader.vue'
+import AppInput from './components/AppInput.vue'
+import AppList from './components/AppList.vue'
+import AppFooter from './components/AppFooter.vue'
 
 export default {
   name: 'App',
   components: {
+    AppHeader,
+    AppInput,
+    AppList,
+    AppFooter
   }
 }
 </script>
---
+++ vueTutorial/self-todo-list/src/components/AppInput.vue
@@ -0,0 +1,13 @@
+<template>
+    <div></div>
+</template>
+
+<script>
+export default {
+    name: "AppInput"
+}
+</script>
+
+<style>
+
+</style>

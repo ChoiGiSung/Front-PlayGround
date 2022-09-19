bac316d07a1d8d2a4e48e281d8eeaa64232f9157
DESKTOP-FL6VBV6\chlrl <chlrltjd159@naver.com>
2022-09-19 22:46:08 +0900

    add login vie

--- vueTILProject/til-project/src/App.vue
+++ vueTILProject/til-project/src/App.vue
@@ -1,19 +1,20 @@
 <template>
   <div id="app">
     <HeaderView/>
-
+    <LoginView/>
   </div>
 </template>
 
 <script>
 import HeaderView from "./components/HeaderView.vue"
-
+import LoginView from "./components/LoginView.vue"
 
 
 export default {
   name: 'App',
   components: {
-    HeaderView
+    HeaderView,
+    LoginView
   }
 }
 </script>
--- vueTILProject/til-project/src/components/LoginView.vue
+++ vueTILProject/til-project/src/components/LoginView.vue
@@ -1,5 +1,44 @@
 <template>
-  <div>로그인</div>
+   <div>
+    <b-form>
+      <b-form-group
+        description="아이디을 씁시다."
+        label= "아이디"
+      >
+        <b-form-input
+          id="input-1"
+          type="text"
+          placeholder="아이디"
+        ></b-form-input>
+      </b-form-group>
+
+      <b-form-group id="input-group-2" 
+       label="비밀번호"
+       description="비밀번호를 씁시다."
+       label-for="input-2">
+        <b-form-input
+          id="input-2"
+          placeholder="비밀번호를 씁시다"
+        ></b-form-input>
+      </b-form-group>
+
+      <b-form-group id="input-group-4">
+        <b-form-checkbox-group
+          id="checkboxes-4"
+        >
+          <b-form-checkbox value="male">남성</b-form-checkbox>
+          <b-form-checkbox value="female">여성</b-form-checkbox>
+        </b-form-checkbox-group>
+      </b-form-group>
+
+      <b-button type="submit">Submit</b-button>
+      <b-button type="reset">Reset</b-button>
+    </b-form>
+    <b-card class="mt-3">
+        <p>card</p>
+    </b-card>
+  </div>
+
 </template>
 
 <script>

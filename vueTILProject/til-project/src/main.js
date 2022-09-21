1ae629a6da48f24f3ed0d5cf689db6cbdcf5d378
DESKTOP-FL6VBV6\chlrl <chlrltjd159@naver.com>
2022-09-21 21:22:11 +0900

    add axios dependency

--- vueTILProject/til-project/src/main.js
+++ vueTILProject/til-project/src/main.js
@@ -4,10 +4,12 @@ import BootstrapVue from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 import router from './router/router'
+import axios from 'axios'
 
 Vue.use(BootstrapVue)
 
 Vue.config.productionTip = false
+Vue.prototype.$http = axios
 
 new Vue({
   render: h => h(App),
